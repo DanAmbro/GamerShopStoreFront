7126f90066cff0c935c0d325753995b410a7e192
DanielAmbro <DanielAmbro1@outlook.com>
2023-03-07 11:34:01 -0600

    Updating the Template

--- GamerShopStoreFront.UI.MVC/Models/AdministrationViewModel.cs
+++ GamerShopStoreFront.UI.MVC/Models/AdministrationViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace [YourProjectName].UI.MVC.Models
+namespace GamerShopStoreFront.UI.MVC.Models
 {
     public class CreateRoleViewModel
     {
