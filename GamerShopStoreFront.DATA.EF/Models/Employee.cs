2d5961c9f4a06ea086c603542cae6e564fdcd189
DanielAmbro <DanielAmbro1@outlook.com>
2023-03-07 21:30:39 -0600

    Added Entity Framework

---
+++ GamerShopStoreFront.DATA.EF/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GamerShopStoreFront.DATA.EF.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string? Title { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string ZipCode { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+}
